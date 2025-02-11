72ef1087d5fc479b6d784cb2f6aa0bca0976327a
Alvaro  Villena <alvarovillena@MacBook-Air.local>
2025-02-10 23:26:54 -0300

    0.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [react()],
@@ -9,6 +8,4 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'static', // Ensures Astro builds static files
-  adapter: cloudflare(),
 });
